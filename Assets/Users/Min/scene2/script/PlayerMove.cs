badc71dbcde019794354784e65004f34e869fa9f
minhtet97 <minhtetnai97@gmail.com>
2022-05-10 03:04:50 +0900

    ゲームシン,　Ui　テキスト,　プレイヤーの当たり,　岩を壊す

---
+++ Assets/Users/Min/scene2/script/PlayerMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    [Header("Player Speed")]
+    public float Speed = 10.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * Speed, vertical * Speed);   
+    }
+}
