facba1e58a7afa4f9bc20005a7f85db28187da34
minhtet97 <minhtetnai97@gmail.com>
2022-05-14 00:46:42 +0900

    enemy follow 2nd test

--- Assets/Users/Min/crash the rock/script/walkmove.cs
+++ Assets/Users/Min/crash the rock/script/walkmove.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class walkmove : MonoBehaviour
 {
     public float speed = 0.1f;
+    Rigidbody2D rb;
+    public bool playerMove = false;
+    public GameObject player;
    void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * speed;
+        if(playerMove == false)
+        {
+            Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+            transform.position = transform.position + horizontal * speed;
+        }
+        
+        
     }
 }
--- Assets/Users/Min/scene2/script/PlayerMove.cs
+++ Assets/Users/Min/scene2/script/PlayerMove.cs
@@ -9,12 +9,19 @@ public class PlayerMove : MonoBehaviour
     float vertical;
     [Header("Player Speed")]
     public float Speed = 10.0f;
+
+    [Header("Text true or false")]
+    public bool touching = false;
+    
+    [Header("Text GameOject")]
+    public GameObject Canvasbar;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
+        Canvasbar.SetActive(false);        
     }
 
     // Update is called once per frame
@@ -22,10 +29,42 @@ public class PlayerMove : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+         if(touching)
+        {
+            Canvasbar.SetActive(true);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * Speed, vertical * Speed);   
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            touching = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            touching = false;
+            Canvasbar.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Destroy(GameObject.FindWithTag("Enemy"));
+            }
+        } 
     }
 }
