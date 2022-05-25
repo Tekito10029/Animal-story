9462172d326d95a40d43d7da8d69497c3d6f529a
minhtet97 <minhtetnai97@gmail.com>
2022-05-26 00:59:20 +0900

    3回目テスティング

--- Assets/Users/Min/crash the rock/script/EnemyController.cs
+++ Assets/Users/Min/crash the rock/script/EnemyController.cs
@@ -4,146 +4,46 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public GameObject Player;
+  /*  public GameObject Player;
     public GameObject enemy;
-    public float stopDistance;         //�~�܂�Ƃ��̋���
-
+    public float stopDistance;
     public PlayerController mt;
+    public bool isCharging = true;
 
-    // ���b�N�ŌF�q:�[�d�\���ǂ����𔻕ʂ���t���O
-    public bool isCharging = true; // HP��0�ɂȂ�����true�ɂ���悤�ɂ��Ă�������
-
-    public bool isFollowing = true;   //�Ǐ]���邩�ǂ���
-    public bool enemyMove = true;      //�G�l�~�[�̓���
-    private bool enemyJump = false;         //�W�����v�p
-    [SerializeField] private bool Follow = false;       //��x�ڂ̓��͂ł̂��Ă��邩�ۂ�
-
+    public bool isFollowing = true;
+    public bool enemyMove = true;
     Rigidbody2D rb2d;
-
     public float inputSpeed;
-    public float jumpingPower;
-    
 
-    //public LayerMask CollisionLayer;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        enemyJump = false;
-    }
-    //�����ɒ����܂ŃW�����v�����Ȃ��}��
-
-    // Start is called before the first frame update
     void Start()
     {
         this.rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        //Vector2 targetPos = Player.transform.position;
+        //targetPos.y = transform.position.y;
+
+       // float distance = Vector2.Distance(transform.position, Player.transform.position);
+        
+    }*/
+
+    public float speed;
+    public float StopDistance;
+    private Transform target;
+
+    void Start()
+    {
+       target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        //����p�ӂ��āA���̒���Y���W������
-        Vector2 targetPos = Player.transform.position;
-        targetPos.y = transform.position.y;
-
-        //����
-        float distance = Vector2.Distance(transform.position, Player.transform.position);
-
-        if (isFollowing)
-        {
-            //if(�Ԃ̋������~�܂�Ƃ��̋����ȏ�Ȃ�?)
-            if (distance > stopDistance)
+        if(Vector2.Distance(transform.position, target.position) > StopDistance)
             {
-                transform.position = Vector3.MoveTowards(transform.position,
-                new Vector2(Player.transform.position.x, enemy.transform.position.y),
-                inputSpeed * Time.deltaTime);
+                    transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             }
-            //enemy��player
-
-            // �E
-            if (Player.transform.position.x < transform.position.x)
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-            }
-
-            // ��
-            else if (Player.transform.position.x > transform.position.x)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-
-            //�W�����v
-            if (enemyJump == false && Input.GetKeyDown(KeyCode.Space))
-            {
-                this.rb2d.AddForce(transform.up * this.jumpingPower);
-                enemyJump = !enemyJump;
-            }
-        }
-
-        //�G�l�~�[�̓����p
-        if (enemyMove == false)
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                this.transform.Translate(-0.01f, 0.0f, 0.0f);
-                transform.localScale = new Vector3(-1, 1, 1);
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                this.transform.Translate(0.01f, 0.0f, 0.0f);
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-
-            if (enemyJump == false && Input.GetKeyDown(KeyCode.Space))
-            {
-                this.rb2d.AddForce(transform.up * this.jumpingPower);
-                enemyJump = !enemyJump;
-            }
-        }
-
-        //������̐؂�ւ�����
-        //1��ڂ̐؂�ւ����̓���
-        if (Input.GetKeyDown(KeyCode.F) && Follow == false)
-        {
-            mt.player_Move = !mt.player_Move;
-            Following();
-            enemyMove = !enemyMove;
-            Follow = !Follow;
-        }
-
-        //2��ڂ̐؂�ւ����A�v���C���[���������ăG�l�~�[�s����
-        //���̏�Ԃ��Ɖ���Enter�����Ă��v���C���[�����������
-        else if (Input.GetKeyDown(KeyCode.F) && Follow == true)
-        {
-            isFollowing = false;
-            enemyMove = true;
-            mt.player_Move = false;
-        }
-
-        //�Ăԃ{�^��(Delete���u��)�����������̓���
-        //Follow��؂�ւ��邱�Ƃł�����x�Ǐ]��؂�ւ����ł��邨
-        if (Follow == true && Input.GetKeyDown(KeyCode.Delete))
-        {
-            isFollowing = true;
-            Follow = !Follow;
-        }
     }
-
-    // ���ĒǏ]�̐؂�ւ�����������
-    public void Following()
-    {
-        isFollowing = !isFollowing;
-    }
-
-    // ���đ���̐؂�ւ�����������
-    public void PlayerChange()
-    {
-        // �v���C���[�̑�����ł��Ȃ�����
-        mt.player_Move = !mt.player_Move;
-
-        // ���쌠��G�Ɉړ�������
-        Following();
-        enemyMove = !enemyMove;
-    }
-
 }
--- Assets/Users/Min/crash the rock/script/PlayerController.cs
+++ Assets/Users/Min/crash the rock/script/PlayerController.cs
@@ -6,294 +6,43 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-
     public GameObject Player;
-
-    private float x_val;
-    private float speed;
-
-    //�v���C���[�̓���̐��l����́i�����A�W�����v�j
-    public float inputSpeed;
-    public float jumpingPower;
-    //
-    public LayerMask CollisionLayer;
-    [SerializeField] private LayerMask enemyLayer; // ���b�N�ŌF�q:�G��Layer�擾�p
-    private bool jumpFlg = false;
-
-    //public Vector2 Speed = new Vector2(1, 1);   //���x
-    private int presskeyFrames = 0;             //�������t���[����
-    private int PressLong = 300;                 //�����������臒l
-    private int PressShort = 100;                //�y�������������臒l
-    private bool aa = false;
-    Item item;
-
-    [SerializeField] int maxHP = 100;
-    [SerializeField] float HP = 100;
-    [SerializeField] private bool touchFlag = false;
-    [SerializeField] private bool enemyTouchFlag = false; // ���b�N�ŌF�q:�t���O�ǉ�
-    public GameObject hpCanvas;
-    private float hpCanvasScale_x;
-
-    public bool player_Move = false;
-
-    private bool enemyFollowFlg = false;
-
     public GameObject enemy;
-
-    //public EnemyController enemyCon;
-
-    // ���b�N�ŌF�q:GetCompornent�d����Œ��Ŏ擾�A�����G�̐�������͂��Ȃ̂ŏ��������邱��
+    public GameObject hpCanvas;
+    public float speed = 1f;
+    [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private EnemyController enemyCon;
     [SerializeField] private Image hp;
+    [SerializeField] private bool touchFlag = false;
+    [SerializeField] private bool enemyTouchFlag = false;
+    
 
-    // Start is called before the first frame update
-    void Start()
+     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        hpCanvasScale_x = hpCanvas.transform.localScale.x;
-        // �F�q:��������Ȃ��Ǝv����
-        //enemy = GameObject.Find("Enemy");
+        hpCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // ���b�N�ŌF�q:Layer�ł���Ă����ۂ��̂�Linecast�Ŏ擾
-        if (GetEnemyLayer())
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * speed;
+
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
         {
-            if (enemyCon.isCharging)
-            {
-                enemyTouchFlag = true;
-            }
-        }
-        else
-        {
-            enemyTouchFlag = false;
-        }
-
-
-        /*�v���C���[�̈ړ����͏���--------------------------------------------*/
-        if(player_Move == false)
-        {
-            //���L�[�������ꂽ�ꍇ
-            x_val = Input.GetAxis("Horizontal");
-            jumpFlg = IsCollision();
-            //Space�L�[�������ꂽ�ꍇ
-            if (Input.GetKeyDown("space") && jumpFlg)
-            {
-                jump();
-            }
-        }
-        
-        /*-----------------------------------------------------------------*/
-
-        /*�E���A������̓��͏���----------------------------------------------------*/
-        if (aa)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                //�X�y�[�X�̔���
-                presskeyFrames += (Input.GetKey(KeyCode.LeftShift)) ? 1 : 0;
-                Debug.Log(presskeyFrames);
-            }
-
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                //�����X�y�[�X�����������ꂽ�獂�߂ɓ�����
-                if (PressLong <= presskeyFrames)
-                {
-                    item.Hight();
-                    Debug.Log("����");
-                    this.gameObject.transform.DetachChildren();
-                }
-                //�����X�y�[�X�������ꂽ���߂ɓ�����
-                else if (PressShort <= presskeyFrames)
-                {
-                    item.Low();
-                    Debug.Log("�Z��");
-                    this.gameObject.transform.DetachChildren();
-                }
-            }
-
-            if (Input.GetKeyUp(KeyCode.W))
-            {
-                this.gameObject.transform.DetachChildren();
-            }
-        }
-        /*-----------------------------------------------------------------*/
-
-        /*�̗͂̌�������-----------------------------------------------------------------*/
-        if (touchFlag || enemyTouchFlag)
-        {
-            // �\��
             hpCanvas.SetActive(true);
-
-            // �d�C�𗬂�
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                HP -= 30;// HP�����炷
-                // ���b�N�ŌF�q:HP�o�[
-                hp.fillAmount = HP / maxHP;
-                Debug.Log(HP);
-                
-                // ���b�N�ŌF�q:�ǉ����܂���
-                // �G��Ă��镨��Enemy�̏ꍇ
-                if (enemyTouchFlag)
-                {
-                    // �Ǐ]�J�n
-                    enemyCon.isFollowing = true;
-                    // �[�d�����̂ł���ȏ�[�d�o���Ȃ��悤��
-                    enemyCon.isCharging = false;
-                    hpCanvas.SetActive(false);
-                }
-                
-            }
-            // �d�C���[�d
-            if (Input.GetKeyDown(KeyCode.RightShift))
-            {
-                HP += 30;// HP�𑝂₷
-                Debug.Log(HP);
-                // �����ɏ�����������
-
-                if(enemyFollowFlg)
-                {
-
-                }
-            }
         }
-        // ���b�N�ŌF�q:HP�\������Object���痣�ꂽ�狭���I��HP�o�[���\���ɂ��܂�
-        else
-        {
-            hpCanvas.SetActive(false);
-        }
-        /*-----------------------------------------------------------------*/
     }
-
-    /*�v���C���[�̕�������--------------------------------------------------*/
-    void FixedUpdate()
-    {
-        //�ҋ@
-        if (x_val == 0)
-        {
-            speed = 0;
-        }
-        //�E�Ɉړ�
-        else if (x_val > 0)
-        {
-            speed = inputSpeed;
-            transform.localScale = new Vector3(1, 1, 1);//�E����������
-            // ���b�N�ŌF�q:HP�o�[�̌����̒���
-            Vector3 hpTransform = new Vector3(hpCanvasScale_x, hpCanvas.transform.localScale.y, hpCanvas.transform.localScale.z);
-            hpCanvas.transform.localScale = hpTransform;
-        }
-        //���Ɉړ�
-        else if (x_val < 0)
-        {
-            speed = inputSpeed * -1;
-            transform.localScale = new Vector3(-1, 1, 1);//������������
-            // ���b�N�ŌF�q:HP�o�[�̌����̒���
-            Vector3 hpTransform = new Vector3(-hpCanvasScale_x, hpCanvas.transform.localScale.y, hpCanvas.transform.localScale.z);
-            hpCanvas.transform.localScale = hpTransform;
-        }
-        // �L�����N�^�[���ړ� Vextor2(x���X�s�[�h�Ay���X�s�[�h(���̂܂�))
-        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
-    }
-    /*-----------------------------------------------------------------*/
-
-
-    /*-----------------------------------------------------*/
-    void jump()
-    {
-        rb2d.AddForce(Vector2.up * jumpingPower);
-        jumpFlg = false;
-    }
-    /*------------------------------------------------------------------*/
-
-    /*�����W�����v��h������------------------------------------------------*/
-    bool IsCollision()
-    {
-        Vector3 left_SP = transform.position - Vector3.right * 0.2f;
-        Vector3 right_SP = transform.position + Vector3.right * 0.2f;
-        Vector3 EP = transform.position - Vector3.up * 1.3f;
-        return Physics2D.Linecast(left_SP, EP, CollisionLayer)
-               || Physics2D.Linecast(right_SP, EP, CollisionLayer);
-    }
-    /*-------------------------------------------------------------------*/
-
-    /// <summary>
-    /// �G�̃��C���[�����邩�ǂ������擾����֐�
-    /// </summary>
-    /// <returns></returns>
-    private bool GetEnemyLayer()
-    {
-        Vector3 left = transform.position + Vector3.up * 1f - Vector3.right * 3.5f;
-        Vector3 right = transform.position + Vector3.up * 1f + Vector3.right * 3.5f;
-        // �����̃R�����g�����΃f�o�b�O�p�̐��������܂�
-        //Debug.DrawLine(left, right);
-        return Physics2D.Linecast(left, right, enemyLayer);
-    }
-
-
-    /*---------------------------*/
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Item")
+        if(collision.gameObject.tag == "Enemy")
         {
-            aa = false;
-            Debug.Log("exit");
-        }
-    }
-    /*-------------------------------------------------------------------*/
-
-    /*-------------------------------------------------------------------*/
-    //�A�C�e���ɓ����葱������
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Item")
-        {
-            Debug.Log("stay");
-
-            //W�������Ă�����
-            if (Input.GetKey(KeyCode.W))
-            {
-                aa = true;
-                //�A�C�e���N���X�̎擾
-                item = collision.gameObject.GetComponent<Item>();
-
-                //�A�C�e����Y�����オ��
-                // �����ł��̃I�u�W�F�N�g���v���C���[�̎q���ɂ���
-                item.gameObject.transform.parent = this.transform;
-            }
-
-        }
-
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            this.gameObject.transform.DetachChildren();
-        }
-    }
-    /*-------------------------------------------------------------------*/
-
-    /*HP�o�[��\������^�O�̔���-----------------------------------------*/
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "HomeApp")
-        {
-            touchFlag = true;
-        }   
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "HomeApp")
-        {
-            touchFlag = false;
             hpCanvas.SetActive(false);
         }
-
-        
-
     }
-    /*-------------------------------------------------------------------*/
-
+    
 }
