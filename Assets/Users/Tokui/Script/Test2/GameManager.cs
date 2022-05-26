2e8dee8886aee03591bb17d82a3e7bd16ec51841
Tekito10029 <tekito10029582@gmail.com>
2022-05-26 22:41:14 +0900

    [作業中]シナリオシーンの実装(今回のプッシュはバグあり)

---
+++ Assets/Users/Tokui/Script/Test2/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+// MonoBehaviourを継承することでオブジェクトにコンポーネントとして
+// アタッチすることができるようになる
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text mainText;
+    [SerializeField]
+    private Text nameText;
+    [SerializeField]
+    private GameObject nextPageIcon;
+    [SerializeField]
+    private float captionSpeed = 0.2f;
+
+    private const char SEPARATE_MAIN_START = '「';
+    private const char SEPARATE_MAIN_END = '」';
+
+    private const char SEPARATE_PAGE = '&';
+
+    private Queue<char> _charQueue;
+    private Queue<string> _pageQueue;
+
+    private string _text =
+        "プレイヤー「テキスト表示テスト1」&イノシシ「テキスト表示テスト2」&プレイヤー「テキスト表示テスト3」";
+
+    // 初期化する
+    private void Init()
+    {
+        _pageQueue = SeparateString(_text, SEPARATE_PAGE);
+        ShowNextPage();
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+    
+    private void Update()
+    {
+        // 左(=0)クリックされたらOnClickメソッドを呼び出し
+        if (Input.GetMouseButtonDown(0)) OnClick();
+    }
+
+    // 次のページを表示する
+    private bool ShowNextPage()
+    {
+        if (_pageQueue.Count <= 0) return false;
+        // オブジェクトの表示/非表示を設定する
+        nextPageIcon.SetActive(false);
+        ReadLine(_pageQueue.Dequeue());
+        return true;
+    }
+
+    //文字列を指定した区切り文字ごとに区切り、キューに格納したものを返す
+    private Queue<string> SeparateString(string str, char sep)
+    {
+        string[] strs = str.Split(sep);
+        Queue<string> queue = new Queue<string>();
+        foreach (string l in strs) queue.Enqueue(l);
+        return queue;
+    }
+
+    //1文字を出力する
+    private bool OutputChar()
+    {
+        if (_charQueue.Count <= 0)
+        {
+            nextPageIcon.SetActive(true);
+            return false;
+        }
+        mainText.text += _charQueue.Dequeue();
+        return true;
+    }
+
+    //全文を表示する
+    private void OutputAllChar()
+    {
+        StopCoroutine(ShowChars(captionSpeed));
+        while (OutputChar()) ;
+        nextPageIcon.SetActive(true);
+    }
+
+
+    private void OnClick()
+    {
+        if (_charQueue.Count > 0) OutputAllChar();
+        else
+        {
+            if (!ShowNextPage())
+                // UnityエディタのPlayモードを終了する
+                UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    //文字送りするコルーチン
+    private IEnumerator ShowChars(float wait)
+    {
+        // OutputCharメソッドがfalseを返す(=キューが空になる)までループする
+        while (OutputChar())
+            // wait秒だけ待機
+            yield return new WaitForSeconds(wait);
+        // コルーチンを抜け出す
+        yield break;
+    }
+
+    private void ReadLine(string text)
+    {
+        string[] ts = text.Split(SEPARATE_MAIN_START);
+        string name = ts[0];
+        string main = ts[1].Remove(ts[1].LastIndexOf(SEPARATE_MAIN_END));
+        nameText.text = name;
+        mainText.text = "";
+        _charQueue = SeparateString(main);
+        // コルーチンを呼び出す
+        StartCoroutine(ShowChars(captionSpeed));
+    }
+
+}
