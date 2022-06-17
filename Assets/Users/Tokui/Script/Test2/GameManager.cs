5938c4611e2cd30142ebaa6819237dffeec6062d
Tekito10029 <tekito10029582@gmail.com>
2022-06-17 18:37:26 +0900

    [Add]キャラクターアニメーションの基礎機能追加。

--- Assets/Users/Tokui/Script/Test2/GameManager.cs
+++ Assets/Users/Tokui/Script/Test2/GameManager.cs
@@ -2,64 +2,78 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private Text mainText;
-    [SerializeField]
-    private Text nameText;
-    [SerializeField]
-    private float captionSpeed = 0.2f;
-    [SerializeField]
-    private GameObject nextPageIcon;
-    [SerializeField]
-    private Image backgroundImage;
-    [SerializeField]
-    private string spritesDirectory = "Sprites/";
-    [SerializeField]
-    private GameObject characterImages;
-    [SerializeField]
-    private string prefabsDirectory = "Prefabs/";
+    [SerializeField] private Text mainText;
+    [SerializeField] private Text nameText;
+    [SerializeField] private float captionSpeed = 0.2f;
+    [SerializeField] private GameObject nextPageIcon;
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private string spritesDirectory = "Sprites/";
+    [SerializeField] private GameObject characterImages;
+    [SerializeField] private string prefabsDirectory = "Prefabs/";
     private List<Image> _charaImageList = new List<Image>();
-    
+    [SerializeField] private string animationsDirectory = "Animations/";
+    [SerializeField] private string overrideAnimationClipName = "Clip";
+    [SerializeField] private string textFile = "Texts/Scenario";
     [SerializeField]
-    private string textFile = "Texts/Scenario";
+    private GameObject selectButtons;
+    private List<Button> _selectButtonList = new List<Button>();
     
     private string _text = "";
-    
+
     private const char SEPARATE_COMMAND = '!';
     private const char COMMAND_SEPARATE_PARAM = '=';
     private const string COMMAND_BACKGROUND = "background";
     private const string COMMAND_SPRITE = "_sprite";
     private const string COMMAND_COLOR = "_color";
-    
     private const string COMMAND_CHARACTER_IMAGE = "charaimg";
     private const string COMMAND_SIZE = "_size";
     private const string COMMAND_POSITION = "_pos";
     private const string COMMAND_ROTATION = "_rotate";
     private const string CHARACTER_IMAGE_PREFAB = "CharacterImage";
-
     private const string COMMAND_ACTIVE = "_active";
     private const string COMMAND_DELETE = "_delete";
+    private const char COMMAND_SEPARATE_ANIM = '%';
+    private const string COMMAND_ANIM = "_anim";
+    private const string COMMAND_JUMP = "jump_to";
+    private const string COMMAND_SELECT = "select";
+    private const string COMMAND_TEXT = "_text";
     
     private const char SEPARATE_MAIN_START = '「';
     private const char SEPARATE_MAIN_END = '」';
-    
     private const char SEPARATE_PAGE = '&';
     
+    private const string SELECT_BUTTON_PREFAB = "SelectButton";
+
     private Queue<string> _pageQueue;
-    
+
     private Queue<char> _charQueue;
+
+    private const char SEPARATE_SUBSCENE = '#';
+    private Dictionary<string, Queue<string>> _subScenes =
+        new Dictionary<string, Queue<string>>();
     
     //初期化する
     private void Init()
     {
         _text = LoadTextFile(textFile);
-        _pageQueue = SeparateString(_text, SEPARATE_PAGE);
+        Queue<string> subScenes = SeparateString(_text, SEPARATE_SUBSCENE);
+        foreach (string subScene in subScenes)
+        {
+            if (subScene.Equals("")) continue;
+            Queue<string> pages = SeparateString(subScene, SEPARATE_PAGE);
+            _subScenes[pages.Dequeue()] = pages;
+        }
+        _pageQueue = _subScenes.First().Value;
         ShowNextPage();
     }
-    
+
     private void Start()
     {
         Init();
@@ -70,7 +84,7 @@ public class GameManager : MonoBehaviour
         // 左(=0)クリックされたらOnClickメソッドを呼び出し
         if (Input.GetMouseButtonDown(0)) OnClick();
     }
-    
+
     //テキストファイルを読み込む
     private string LoadTextFile(string fname)
     {
@@ -84,7 +98,7 @@ public class GameManager : MonoBehaviour
         string p = parameter.Replace(" ", "");
         return p.Equals("true") || p.Equals("TRUE");
     }
-    
+
     //立ち絵の設定
     private void SetCharacterImage(string name, string cmd, string parameter)
     {
@@ -93,13 +107,15 @@ public class GameManager : MonoBehaviour
         Image image = _charaImageList.Find(n => n.name == name);
         if (image == null)
         {
-            image = Instantiate(Resources.Load<Image>(prefabsDirectory + CHARACTER_IMAGE_PREFAB), characterImages.transform);
+            image = Instantiate(Resources.Load<Image>(prefabsDirectory + CHARACTER_IMAGE_PREFAB),
+                characterImages.transform);
             image.name = name;
             _charaImageList.Add(image);
         }
+
         SetImage(cmd, parameter, image);
     }
-    
+
     //パラメーターからベクトルを取得する
     private Vector3 ParameterToVector3(string parameter)
     {
@@ -113,7 +129,7 @@ public class GameManager : MonoBehaviour
         cmd = cmd.Replace(COMMAND_BACKGROUND, "");
         SetImage(cmd, parameter, backgroundImage);
     }
-    
+
     //コマンドの読み出し
     private void ReadCommand(string cmdLine)
     {
@@ -126,15 +142,36 @@ public class GameManager : MonoBehaviour
                 SetBackgroundImage(cmds[0], cmds[1]);
             if (cmds[0].Contains(COMMAND_CHARACTER_IMAGE))
                 SetCharacterImage(cmds[1], cmds[0], cmds[2]);
+            if (cmds[0].Contains(COMMAND_JUMP))
+                JumpTo(cmds[1]);
+            if (cmds[0].Contains(COMMAND_SELECT))
+                SetSelectButton(cmds[1], cmds[0], cmds[2]);
         }
     }
     
+    
+    //選択肢の設定
+    private void SetSelectButton(string name, string cmd, string parameter)
+    {
+        cmd = cmd.Replace(COMMAND_SELECT, "");
+        name = name.Substring(name.IndexOf('"') + 1, name.LastIndexOf('"') - name.IndexOf('"') - 1);
+        Button button = _selectButtonList.Find(n => n.name == name);
+        if (button == null)
+        {
+            button = Instantiate(Resources.Load<Button>(prefabsDirectory + SELECT_BUTTON_PREFAB), selectButtons.transform);
+            button.name = name;
+            button.onClick.AddListener(() => SelectButtonOnClick(name));
+            _selectButtonList.Add(button);
+        }
+        SetImage(cmd, parameter, button.image);
+    }
+
     //スプライトをファイルから読み出し、インスタンス化する
     private Sprite LoadSprite(string name)
     {
         return Instantiate(Resources.Load<Sprite>(spritesDirectory + name));
     }
-    
+
     //パラメーターから色を作成する
     private Color ParameterToColor(string parameter)
     {
@@ -146,7 +183,7 @@ public class GameManager : MonoBehaviour
             return new Color32(byte.Parse(ps[0]), byte.Parse(ps[1]),
                 byte.Parse(ps[2]), 255);
     }
-    
+
     //画像の設定
     private void SetImage(string cmd, string parameter, Image image)
     {
@@ -176,9 +213,12 @@ public class GameManager : MonoBehaviour
                 _charaImageList.Remove(image);
                 Destroy(image.gameObject);
                 break;
+            case COMMAND_ANIM:
+                ImageSetAnimation(image, parameter);
+                break;
         }
     }
-    
+
     //文字列を指定した区切り文字ごとに区切り、キューに格納したものを返す
     private Queue<string> SeparateString(string str, char sep)
     {
@@ -187,7 +227,7 @@ public class GameManager : MonoBehaviour
         foreach (string l in strs) queue.Enqueue(l);
         return queue;
     }
-    
+
     //次のページを表示する
     private bool ShowNextPage()
     {
@@ -197,7 +237,7 @@ public class GameManager : MonoBehaviour
         ReadLine(_pageQueue.Dequeue());
         return true;
     }
-    
+
     //文を1文字ごとに区切り、キューに格納したものを返す
     private Queue<char> SeparateString(string str)
     {
@@ -209,7 +249,7 @@ public class GameManager : MonoBehaviour
         foreach (char c in chars) charQueue.Enqueue(c);
         return charQueue;
     }
-    
+
     //1文字を出力する
     private bool OutputChar()
     {
@@ -218,10 +258,11 @@ public class GameManager : MonoBehaviour
             nextPageIcon.SetActive(true);
             return false;
         }
+
         mainText.text += _charQueue.Dequeue();
         return true;
     }
-    
+
     //全文を表示する
     private void OutputAllChar()
     {
@@ -229,16 +270,126 @@ public class GameManager : MonoBehaviour
         while (OutputChar()) ;
         nextPageIcon.SetActive(true);
     }
-    
+
+    //パラメーターからアニメーションクリップを生成する
+    private AnimationClip ParameterToAnimationClip(Image image, string[] parameters)
+    {
+        string[] ps = parameters[0].Replace(" ", "").Split(',');
+        string path = animationsDirectory + SceneManager.GetActiveScene().name + "/" + image.name;
+        AnimationClip prevAnimation = Resources.Load<AnimationClip>(path + "/" + ps[0]);
+        AnimationClip animation;
+#if UNITY_EDITOR
+        if (ps[3].Equals("Replay") && prevAnimation != null)
+            return Instantiate(prevAnimation);
+        animation = new AnimationClip();
+        Color startcolor = image.color;
+        Vector3[] start = new Vector3[3];
+        start[0] = image.GetComponent<RectTransform>().sizeDelta;
+        start[1] = image.GetComponent<RectTransform>().anchoredPosition;
+        Color endcolor = startcolor;
+        if (parameters[1] != "") endcolor = ParameterToColor(parameters[1]);
+        Vector3[] end = new Vector3[3];
+        for (int i = 0; i < 2; i++)
+        {
+            if (parameters[i + 2] != "")
+                end[i] = ParameterToVector3(parameters[i + 2]);
+            else end[i] = start[i];
+        }
+
+        AnimationCurve[,] curves = new AnimationCurve[4, 4];
+        if (ps[3].Equals("EaseInOut"))
+        {
+            curves[0, 0] = AnimationCurve.EaseInOut(float.Parse(ps[1]), startcolor.r, float.Parse(ps[2]), endcolor.r);
+            curves[0, 1] = AnimationCurve.EaseInOut(float.Parse(ps[1]), startcolor.g, float.Parse(ps[2]), endcolor.g);
+            curves[0, 2] = AnimationCurve.EaseInOut(float.Parse(ps[1]), startcolor.b, float.Parse(ps[2]), endcolor.b);
+            curves[0, 3] = AnimationCurve.EaseInOut(float.Parse(ps[1]), startcolor.a, float.Parse(ps[2]), endcolor.a);
+            for (int i = 0; i < 2; i++)
+            {
+                curves[i + 1, 0] =
+                    AnimationCurve.EaseInOut(float.Parse(ps[1]), start[i].x, float.Parse(ps[2]), end[i].x);
+                curves[i + 1, 1] =
+                    AnimationCurve.EaseInOut(float.Parse(ps[1]), start[i].y, float.Parse(ps[2]), end[i].y);
+                curves[i + 1, 2] =
+                    AnimationCurve.EaseInOut(float.Parse(ps[1]), start[i].z, float.Parse(ps[2]), end[i].z);
+            }
+        }
+        else
+        {
+            curves[0, 0] = AnimationCurve.Linear(float.Parse(ps[1]), startcolor.r, float.Parse(ps[2]), endcolor.r);
+            curves[0, 1] = AnimationCurve.Linear(float.Parse(ps[1]), startcolor.g, float.Parse(ps[2]), endcolor.g);
+            curves[0, 2] = AnimationCurve.Linear(float.Parse(ps[1]), startcolor.b, float.Parse(ps[2]), endcolor.b);
+            curves[0, 3] = AnimationCurve.Linear(float.Parse(ps[1]), startcolor.a, float.Parse(ps[2]), endcolor.a);
+            for (int i = 0; i < 2; i++)
+            {
+                curves[i + 1, 0] = AnimationCurve.Linear(float.Parse(ps[1]), start[i].x, float.Parse(ps[2]), end[i].x);
+                curves[i + 1, 1] = AnimationCurve.Linear(float.Parse(ps[1]), start[i].y, float.Parse(ps[2]), end[i].y);
+                curves[i + 1, 2] = AnimationCurve.Linear(float.Parse(ps[1]), start[i].z, float.Parse(ps[2]), end[i].z);
+            }
+        }
+
+        string[] b1 = { "r", "g", "b", "a" };
+        for (int i = 0; i < 4; i++)
+        {
+            AnimationUtility.SetEditorCurve(
+                animation,
+                EditorCurveBinding.FloatCurve("", typeof(Image), "m_Color." + b1[i]),
+                curves[0, i]
+            );
+        }
+
+        string[] a = { "m_SizeDelta", "m_AnchoredPosition" };
+        string[] b2 = { "x", "y", "z" };
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                AnimationUtility.SetEditorCurve(
+                    animation,
+                    EditorCurveBinding.FloatCurve("", typeof(RectTransform), a[i] + "." + b2[j]),
+                    curves[i + 1, j]
+                );
+            }
+        }
+
+        if (!Directory.Exists("Assets/Resources/" + path))
+            Directory.CreateDirectory("Assets/Resources/" + path);
+        AssetDatabase.CreateAsset(animation, "Assets/Resources/" + path + "/" + ps[0] + ".anim");
+        AssetDatabase.ImportAsset("Assets/Resources/" + path + "/" + ps[0] + ".anim");
+#elif UNITY_STANDALONE
+       animation = prevAnimation;
+#endif
+        return Instantiate(animation);
+    }
+
+    //アニメーションを画像に設定する
+    private void ImageSetAnimation(Image image, string parameter)
+    {
+        Animator animator = image.GetComponent<Animator>();
+        AnimationClip clip = ParameterToAnimationClip(image, parameter.Split(COMMAND_SEPARATE_ANIM));
+        AnimatorOverrideController overrideController;
+        if (animator.runtimeAnimatorController is AnimatorOverrideController)
+            overrideController = (AnimatorOverrideController)animator.runtimeAnimatorController;
+        else
+        {
+            overrideController = new AnimatorOverrideController();
+            overrideController.runtimeAnimatorController = animator.runtimeAnimatorController;
+            animator.runtimeAnimatorController = overrideController;
+        }
+
+        overrideController[overrideAnimationClipName] = clip;
+        animator.Update(0.0f);
+        animator.Play(overrideAnimationClipName, 0);
+    }
+
     //クリックしたときの処理
     private void OnClick()
     {
         if (_charQueue.Count > 0) OutputAllChar();
         else
         {
+            if (_selectButtonList.Count > 0) return;
             if (!ShowNextPage())
-                // UnityエディタのPlayモードを終了する
-                UnityEditor.EditorApplication.isPlaying = false;
+                EditorApplication.isPlaying = false;
         }
     }
 
@@ -247,6 +398,7 @@ public class GameManager : MonoBehaviour
         if (text[0].Equals(SEPARATE_COMMAND))
         {
             ReadCommand(text);
+            if (_selectButtonList.Count > 0) return;
             ShowNextPage();
             return;
         }
@@ -264,6 +416,15 @@ public class GameManager : MonoBehaviour
         StartCoroutine(ShowChars(captionSpeed));
     }
     
+    //選択肢がクリックされた
+    private void SelectButtonOnClick(string label)
+    {
+        foreach (Button button in _selectButtonList) Destroy(button.gameObject);
+        _selectButtonList.Clear();
+        JumpTo('"' + label + '"');
+        ShowNextPage();
+    }
+
     //文字送りするコルーチン
     private IEnumerator ShowChars(float wait)
     {
@@ -273,5 +434,12 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(wait);
         // コルーチンを抜け出す
         yield break;
+    }
+    
+    //対応するラベルまでジャンプする
+    private void JumpTo(string parameter)
+    {
+        parameter = parameter.Substring(parameter.IndexOf('"') + 1, parameter.LastIndexOf('"') - parameter.IndexOf('"') - 1);
+        _pageQueue = _subScenes[parameter];
     }
 }
